33e156218deecd4c175371cb089993cbc90346d9
harathik <59468552+harathik@users.noreply.github.com>
2024-12-20 18:55:05 +0530

    Update SpringFoxConfig.java

--- Microservices/MicroService_Booking/src/main/java/com/hotel/MicroService_Reservation/config/SpringFoxConfig.java
+++ Microservices/MicroService_Booking/src/main/java/com/hotel/MicroService_Reservation/config/SpringFoxConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
